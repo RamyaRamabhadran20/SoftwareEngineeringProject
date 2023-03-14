7f9027b9e3caa7fb4454d49bfa84e8660ac330a2
npai302 <59401373+npai302@users.noreply.github.com>
2023-03-14 12:28:03 -0400

    Add files via upload

---
+++ SWEProj/js/landing.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req,res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, '../html/landing.html'), (err, data) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end('404 Not Found');
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        });    
+    }
+    else if (req.url === '/catalog.html') {
+        fs.readFile(path.join(__dirname, '../html/catalog.html'), (err, data) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end('404 Not Found');
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        });    
+    }
+});
+            
+server.listen(8080, () => {
+    console.log('Server running at http://localhost:8080/');
+});
